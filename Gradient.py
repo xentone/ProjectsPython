eb9409a0333263d195bc3a5852d5cb27a21768e3
xentone <58145365+xentone@users.noreply.github.com>
2026-02-05 17:36:00 +0500

    Update Gradient.py

--- Gradient.py
+++ Gradient.py
@@ -9,7 +9,7 @@ img = cv.imread(
 )  # change this path with the path of image passed as an input
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-## Just to check the images obtained in grayscale mode and compare with the original image.
+## just to check the images obtained in grayscale mode and compare with the original image.
 # cv.imshow('image',img)
 # cv.imshow('Gray Image', gray)
 
