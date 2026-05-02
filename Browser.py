fbac635df33f14ee644dd3d7d230698e7630d7d4
xentone <58145365+xentone@users.noreply.github.com>
2026-05-02 20:24:59 +0500

    Update Browser.py

--- Browser.py
+++ Browser.py
@@ -4,7 +4,7 @@ from PyQt5.QtWebEngineWidgets import *
 import sys
 
 
-# Define the main window class
+# define the main window class
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
